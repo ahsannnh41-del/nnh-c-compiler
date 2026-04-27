b9b06c238885839416b62bcc53bfd7d1c7a54bcc
NONAMEHACKER <ahsannnh41@gmail.com>
2026-04-27 17:42:11 +0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        vscode: {
-          bg: "#1e1e1e",
-          sidebar: "#333333",
-          header: "#252526",
-          blue: "#007acc",
-          border: "#444444"
-        }
-      }
-    },
+    extend: {},
   },
   plugins: [],
 }
