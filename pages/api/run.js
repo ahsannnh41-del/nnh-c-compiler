b78e595944f3329e355f2891317268c875c4f346
NONAMEHACKER <ahsannnh41@gmail.com>
2026-04-27 17:20:44 +0500

    Update run.js

--- pages/api/run.js
+++ pages/api/run.js
@@ -1,1 +1,29 @@
+export default async function handler(req, res) {
+  const { code } = req.body;
 
+  try {
+    const response = await fetch("https://emkc.org/api/v2/piston/execute", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        language: "c",
+        version: "10.2.0",
+        files: [{ content: code }],
+      }),
+    });
+
+    const data = await response.json();
+
+    const output =
+      data.run?.output ||
+      data.compile?.output ||
+      "No Output / Error";
+
+    res.status(200).json({ output });
+
+  } catch (err) {
+    res.status(200).json({
+      output: "Server Error: " + err.message,
+    });
+  }
+}
